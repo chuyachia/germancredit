71b1b4e623b15b88be7d1c91dd819dd84f8bfa4d
chuyachia <chuyachia@gmail.com>
2017-07-01 17:18:08 -0400

    use up-sampled data

--- mlGermanCredit.R
+++ mlGermanCredit.R
@@ -1,13 +1,9 @@
 #### This code uses svm with gaussian kernal, gradient boosted tree and
-#### elastic net respectively on the original and the up-sampled data to 
-#### create a total of 6 models. The 6 models are then blended together using
-#### using a simple glm to create the final model
-#### 5-fold cv is used to determine hyperparameters of the models and to 
-#### evaluate the accuracy and the weighted penalty of the model according
-#### to the given penalty matrix 
-#### The whole process is run 10 times with different randomly chosen seeds.
-#### In most of the time, the final model obtained by blending the 6 models
-#### achieves higher accuracy and lower penalty than any of the 6 models.
+#### elastic net respectively on an up-sampled data to create 3 base models. 
+#### The 3 base models are then blended together using a simple glm to 
+#### create the final model.
+wd = "C:/Users/Client/Desktop/"
+setwd(wd)
 
 library(caret)
 data(GermanCredit)
@@ -23,181 +19,131 @@ X <- X[,-X_ldinfo$remove]
 #### Preprocess control ####
 preproc <- c("nzv","zv","scale","center")
 
-#### Custom metrics (Accuracy+WeightedPenalty ####
+#### Custom metrics  ####
 # Positive being "Bad"
-false_neg_cost = 5
-false_pos_cost = 1
+weightedAccu <- function(obs,pred,positive,false_pos_cost,false_neg_cost) {
+return(1-(sum(ifelse(obs==positive,
+                     false_neg_cost*(obs!=pred),
+                     false_pos_cost*(obs!=pred)))/
+            (sum(obs==positive)*false_neg_cost+sum(obs!=positive))))  
+}
 
 customM <- function(data, lev = NULL, model = NULL) {
-  penalty<- mean(ifelse(data[, "obs"]=="Bad",
-                        false_neg_cost*(data[, "obs"]!=data[, "pred"]),
-                        false_pos_cost*(data[, "obs"]!=data[, "pred"])))
+  accu_w <- weightedAccu(obs=data[,"obs"],pred=data[,"pred"],
+                         positive="Bad",
+                         false_pos_cost=1,
+                         false_neg_cost=5)
   accu <- mean(data[, "obs"]==data[, "pred"])
-  c(Accuracy= accu,WeightedPenalty=penalty)
+  precis <- (sum(data[, "pred"]=="Bad"&data[, "obs"]=="Bad")/sum(data[, "pred"]=="Bad"))
+  recall <- (sum(data[, "pred"]=="Bad"&data[, "obs"]=="Bad")/sum(data[, "obs"]=="Bad"))
+  c(WeightedAccuracy=accu_w,Accuracy= accu,Precision= precis,Recall= recall)
 }
 
 
 #### Training control ####
 fitControl<- trainControl(
   method = "cv",
-  number=5,
+  number=10,
   summaryFunction=customM)
 
 fitControlUp <- trainControl(
   method = "cv",
-  number = 5,
+  number = 10,
   sampling="up",
   summaryFunction=customM)
 
-#### Seed ####
-seedlist <- floor(runif(10)*1000)
-for (i in 1:length(seedlist)) {
-#seed <- 70
-seed <- seedlist[i]
-
+#### Train ####
+# 10 rounds, without seed
+#seedlist <- floor(runif(10)*1000)
+n =
+for (i in 1:n) {
+#seed <- seedlist[i]
 #### Split training testing ####
-set.seed(seed)
-Train_indx <- createDataPartition(GermanCredit$Class,p=.75,list=F)
+#set.seed(seed)
+Train_indx <- createDataPartition(GermanCredit$Class,p=.7,list=F)
 Data_train <- cbind(X[Train_indx,],Class=Y[Train_indx])
-Data_test <- cbind(X[-Train_indx,],Class=Y[-Train_indx])
+Data_blend <- cbind(X[-Train_indx,],Class=Y[-Train_indx])
 
 #### SVM radial  ####
-# original data
-set.seed(seed)
-svmgrid <- expand.grid(C=seq(0.1,3,0.1),
-                       sigma=seq(0.01,0.1,0.01))
+#set.seed(seed)
+svmgrid <- expand.grid(C=seq(0.1,3,0.2),
+                       sigma=seq(0.01,0.1,0.02))
 svmradial<-  train(Class ~ ., data = Data_train, 
-                    method = "svmRadial",
-                    trControl=fitControl,
-                    preProcess= preproc,
-                    tuneGrid=svmgrid)
-#svmradial
-#plot(svmradial)
-#svmimportance <- varImp(svmradial)
-#plot(svmimportance,top="20")
-#confusionMatrix(data = predict(svmradial,Data_test), reference = Data_test$Class)
-
-# up sampled train data
-set.seed(seed)
-svmradial2<-  train(Class ~ ., data = Data_train, 
                    method = "svmRadial",
                    trControl=fitControlUp,
                    preProcess= preproc,
-                   tuneGrid=svmgrid)
-#svmradial2
-#svmradial2$bestTune
-#plot(svmradial2)
-#confusionMatrix(data = predict(svmradial2,Data_test), reference = Data_test$Class)
+                   #tuneGrid=svmgrid,
+                   metric="WeightedAccuracy")#,
+                   #class.weights=c("Bad"=5/6,"Good"=1/6))
+#svmradial
+#svmradial$bestTune
+#plot(svmradial)
+#confusionMatrix(data = predict(svmradial,Data_blend), reference = Data_blend$Class)
 
 #### Gradient boosting ####
-# original train data
-set.seed(seed)
-xgb<-  train(Class ~ ., data = Data_train,
-             method = "xgbTree",
-             preProcess=preproc,
-             trControl=fitControl)
+#set.seed(seed)
+xgb<-  train(Class ~ ., data = Data_train, 
+               method = "xgbTree",
+               preProcess=preproc,
+               trControl=fitControlUp,
+               metric="WeightedAccuracy")
+              
 #xgb
 #plot(xgb)
-#xgbimportance <- varImp(xgb)
-#plot(xgbimportance,top="20")
-#confusionMatrix(data = predict(xgb,Data_test), reference = Data_test$Class)
-
-# up sampled train data
-set.seed(seed)
-xgb2<-  train(Class ~ ., data = Data_train, 
-             method = "xgbTree",
-             preProcess=preproc,
-             trControl=fitControlUp)
-#xgb2
-#plot(xgb2)
-#confusionMatrix(data = predict(xgb2,Data_test), reference = Data_test$Class)
+#confusionMatrix(data = predict(xgb,Data_blend), reference = Data_blend$Class)
 
 #### Elastic net ####
-# original data
-set.seed(seed)
+#set.seed(seed)
 enetgrid <-  expand.grid(alpha= seq(0.1,1,0.05),
                         lambda=seq(0.01,0.1,0.01))
 
+#weights <- ifelse(Data_train$Class=="Bad",5,1)
 enet<-  train(Class ~ ., data = Data_train, 
-             method = "glmnet",
-             preProcess=preproc,
-             trControl=fitControl,
-             tuneGrid=enetgrid)
-#enet
-#plot(enet)
-#confusionMatrix(data = predict(enet,Data_test), reference = Data_test$Class)
-#coef(enet$finalModel, enet$bestTune$lambda)
-# up sampled train data
-set.seed(seed)
-enet2<-  train(Class ~ ., data = Data_train, 
               method = "glmnet",
               preProcess=preproc,
               trControl=fitControlUp,
-              tuneGrid= enetgrid)
-#plot(enet2)
-#enet2$bestTune
-#confusionMatrix(data = predict(enet2,Data_train), reference = Data_train$Class)
-#confusionMatrix(data = predict(enet2,Data_test), reference = Data_test$Class)
-#coef(enet$finalModel, enet$bestTune$lambda)
+              tuneGrid= enetgrid,
+              metric="WeightedAccuracy")
+              #weights=weights) #problem : len(weights)!= upsampled x
 
+
+#plot(enet)
+#enet$results
+#enet$bestTune
+#confusionMatrix(data = predict(enet,Data_blend), reference = Data_blend$Class)
+#coef(enet$finalModel, enet$bestTune$lambda)
 
 #### Compare models ####
 # Performance
-#resamps <- resamples(list(Enet=enet,
-#                          Enet2=enet2,
-#                          RadialSVM=svmradial,
-#                          RadialSVM2=svmradial2,
-#                          Xgb1 =xgb,
-#                          Xgb2=xgb2))
-                          
+modelList <- list(RadialSVM=svmradial,
+                 Xgb1 =xgb,
+                 Enet=enet)
+resamps<- resamples(modelList)
+
+
 #bwplot(resamps)
-#dotplot(resamps)
 #summary(resamps)
 #summary(diff(resamps))
 
 # Correlation
 #library(corrplot)
 #modelCor(resamps)
-#corrplot(modelCor(resamps))
+#corrplot(modelCor(resamps),method="number")
 #splom(resamps)
-#findCorrelation(modelCor(resamps))
-
 
 #### Ensemble models ###
-Data_ensem <- data.frame(svm_1=predict(svmradial,Data_test),
-                     svm_2= predict(svmradial2,Data_test),
-                     enet_1=predict(enet,Data_test),
-                     enet_2=predict(enet2,Data_test),
-                     xgb_1=predict(xgb,Data_test),
-                     xgb_2=predict(xgb2,Data_test))
-Data_ensem$Class <- Data_test$Class
-# blending using elastic net (too unstable)
-#enet_blend<-  train(Class ~ ., data = Data_ensem, 
-#                     method = "glmnet",
-#                     trControl=fitControl,
-#                    tuneGrid=enetgrid)
-#enet_blend
-#plot(enet_blend)
-#enet_blend$bestTune
+basemodel <- lapply(modelList,predict,newdata=Data_blend)
+Data_ensem<- do.call(cbind.data.frame, basemodel)
+Data_ensem$Class <- Data_blend$Class
 
-# blending using glm
-glm_blend<-  train(Class ~ ., data = Data_ensem, 
+# blending using random forest
+set.seed(seed)
+blend<-  train(Class ~ ., data = Data_ensem, 
                     method = "glm",
                     trControl=fitControl)
-glm_blend
-#### Compare2 ####
-resamps2 <- resamples(list(Enet=enet,
-                          Enet2=enet2,
-                          RadialSVM=svmradial,
-                          RadialSVM2=svmradial2,
-                          Xgb1 =xgb,
-                          Xgb2=xgb2,
-                          Blended=glm_blend))
-#bwplot(resamps2)
-#dotplot(resamps2)
-#summary(resamps2)
-#class(resamps2)
-out <- capture.output(summary(resamps2))
-cat(paste("Round",i), out, file="C:/Users/Client/Desktop/result.txt", sep="n", fill=TRUE,append=TRUE)
+blend$results
+
+out <- capture.output(Blended=blend$results)
+cat(paste("Round",i), out, file="result.txt", sep="n", fill=TRUE,append=TRUE)
 print(paste("Round",i,"finished!"))
 }
